f3c91bb2d0530c4278720a015411b9c4f6b69b1c
Yuriibe <direxpvp@gmail.com>
2023-09-18 18:50:14 +0200

    tested more and started crying

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import dataManager
 import itemLogger
 
 last_package = ""
-last_payload = b''  # Initialize a variable to hold the last payload
+last_payload = ""  # Initialize a variable to hold the last payload
 all_ips = ["20.76.13", "20.76.14.27",
            "20.76.14", "45.223.19.187", "63.32.251.0", "52.30.70.249"]
 identifier_regex = r"[56][0-9a-f]0100[0-9a-f]{4}"
@@ -23,10 +23,7 @@ def get_price(item_id):
         price = get_vendor_price(item_id)
     return price
 
-
 def package_handler(package, messages):
-    global last_package
-
     if (IP not in package):
         return
     ip = package[IP]
@@ -37,44 +34,53 @@ def package_handler(package, messages):
         if not hasattr(package[TCP].payload, "load"):
             return
 
-
+       # print(package[TCP].payload)
         payload_raw = bytes(package[TCP].payload).hex()
+        content = last_payload + payload_raw
 
-        # Merging the logic for handling split identifiers
-        payload = last_package + payload_raw
         position = 0
-        while(len(payload[position:]) >= 600):
-            payload = payload[position:]
+
+        while len(content[position:]) >= 500:
             position = 0
-            match_location = 0
-            matches = list(re.finditer(identifier_regex, payload))
+            matches = list(re.finditer('0b010006', content))
 
             if len(matches) == 0:
                 return  # no match found, return - could cause issue if the identifier is split between two packages
+                print("No Match")
             elif len(matches) == 1:
                 match_location = matches[0].start()
+                #print("Match 1")
             else:
-                if matches[0].start() + 600 < matches[1].start():
+                if matches[0].start() + 500 < matches[1].start():
                     match_location = matches[0].start()
+                    #print("Match 2")
                 else:
                     match_location = matches[1].start()
+                    #print("Match 3")
+            print(content)
+            identifier_length = len('0b010006')
+            content = content[match_location + identifier_length:]
+            global last_package
+            if last_package == 0:
+                last_package = payload_raw
+                print(last_package)
+            else:
+                length = min(len(last_package), len(payload_raw))
+                differences = 0
+                for i in range(0, length - 1, 2):
+                    if (last_package[i:i + 2] != payload_raw[i:i + 2]):
+                        differences += 1
 
-            payload = payload[match_location:]
+                last_package = payload_raw
+                #print(len(content))
+                #print(content)
+                #print(match_location)
 
-        last_package = payload_raw
-
-        # Continue with the original logic
-        content = payload_raw
-        while '0b010006' in content:
-            index = content.index("0b010006")
-            content = content[index:]
-
-            amount_hex = content[60:64]
-            id_hex = content[54:58]
-
+            amount_hex = content[52:56]
+           # id_hex = content[54:58]
+            id_hex = content[46:50]
             item_id = int(reverse_id(id_hex), 16)
             amount = int(amount_hex, 16)
-
             total_price = int(get_price(item_id)) * int(amount)
             item_name = str(get_item_name(item_id))
             dataManager.set_item_id(item_id)
@@ -87,7 +93,9 @@ def package_handler(package, messages):
             print("Price: " + str(total_price))
             print("\n")
 
-            content = content[index + 2:]
+
+            content = content[match_location:]
+
 
 
 
